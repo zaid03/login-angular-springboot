b1b31d9253b69c0e6161f89e6d9ecc419995f193
zaid03 <terguyzaid@gmail.com>
2025-12-01 17:09:59 +0100

    added search and content table for familias manipulation

--- frontend/src/app/familia/familia.component.ts
+++ frontend/src/app/familia/familia.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-familia',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule ,FormsModule, SidebarComponent],
   templateUrl: './familia.component.html',
-  styleUrl: './familia.component.css'
+  styleUrls: ['./familia.component.css']
 })
 export class FamiliaComponent {
 
