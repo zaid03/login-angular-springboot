8c851c37572f9e5b1a9bc7c5bee77bd4844ae9f6
zaid03 <terguyzaid@gmail.com>
2026-02-08 15:35:06 +0100

    added a new button for cge D to contratos + api for delete centros for contratos

--- backend/src/main/java/com/example/backend/controller/CogController.java
+++ backend/src/main/java/com/example/backend/controller/CogController.java
@@ -1,6 +1,7 @@
 package com.example.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -8,10 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.backend.dto.COGAIPOnlyDto;
 import com.example.backend.dto.CogCgeProjection;
 import com.example.backend.sqlserver2.repository.CogRepository;
-// import com.example.backend.sqlserver2.model.Cog;
-// import com.example.backend.sqlserver2.model.CogId;
+import com.example.backend.sqlserver2.model.CogId;
 
 @RestController
 @RequestMapping("/api/cog")
@@ -33,6 +34,38 @@ public class CogController {
             }
 
             return ResponseEntity.ok(centros);
+        } catch (DataAccessException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Error: " + ex.getMostSpecificCause().getMessage());
+        }
+    }
+
+    //deleting a centro gestor from contrato
+    @DeleteMapping("/delete-centro/{ent}/{eje}/{concod}/{cgecod}")
+    public ResponseEntity<?> deleteCentroGestore(
+        @PathVariable Integer ent,
+        @PathVariable String eje,
+        @PathVariable Integer concod,
+        @PathVariable String cgecod
+    ) {
+        try {
+            Optional<COGAIPOnlyDto> centro =  cogRepository.findByENTAndEJEAndCONCODAndCGECOD(ent, eje, concod, cgecod);
+
+            if (centro.isPresent()) {
+                Double cogaip = centro.get().getCOGAIP();
+                if (cogaip != null && cogaip > 0) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No se puede quitar un centro gestor donde ya hay pedidos’ y no seguimos");
+                } else if (cogaip != null && cogaip == 0) {
+                    CogId id = new CogId(ent, eje, concod, cgecod);
+                    cogRepository.deleteById(id);
+
+                    return ResponseEntity.noContent().build();
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sin resultado");
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sin resultado");
+            }
         } catch (DataAccessException ex) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Error: " + ex.getMostSpecificCause().getMessage());
--- backend/src/main/java/com/example/backend/sqlserver2/repository/CogRepository.java
+++ backend/src/main/java/com/example/backend/sqlserver2/repository/CogRepository.java
@@ -3,11 +3,16 @@ package com.example.backend.sqlserver2.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.sqlserver2.model.Cog;
 import com.example.backend.sqlserver2.model.CogId;
+import com.example.backend.dto.COGAIPOnlyDto;
 import com.example.backend.dto.CogCgeProjection;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CogRepository extends JpaRepository<Cog, CogId> {
     //selecting centro gestores for contrato
     List<CogCgeProjection> findAllByENTAndEJEAndCONCOD(Integer ENT, String EJE, Integer CONCOD);
+
+    //needed for deleting a centro gestor from a contrato
+    Optional<COGAIPOnlyDto> findByENTAndEJEAndCONCODAndCGECOD(Integer ENT, String EJE, Integer CONCOD, String CGECOD);
 }
